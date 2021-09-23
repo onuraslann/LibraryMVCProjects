a14d1ffb478e0c66d3254c025beac3bf12d858f8
Onuraslann <theeaslann1@gmail.com>
2021-09-23 21:04:11 +0300

    Crud Commit

--- LibraryMVCProjects/Controllers/OperationController.cs
+++ LibraryMVCProjects/Controllers/OperationController.cs
@@ -1,4 +1,5 @@
 ﻿using LibraryMVCProjects.Models.EntityFramework;
+using LibraryMVCProjects.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,28 +8,32 @@ using System.Web.Mvc;
 
 namespace LibraryMVCProjects.Controllers
 {
-    public class StudentController : Controller
+    public class OperationController : Controller
     {
         LibraryBackendsEntities db = new LibraryBackendsEntities();
         public ActionResult Index()
         {
-            var model = db.Students.ToList();
+            var model = db.Operations.ToList();
             return View(model);
         }
         public ActionResult Yeni()
         {
-            return View("Yeni");
-
-        }
-        public ActionResult Kaydet(Students students)
-        {
-            if (students.Id == 0)
+            var model = new OperationViewModels()
             {
-                db.Students.Add(students);
+                 Books=db.Books.ToList(),
+                 Students=db.Students.ToList()
+            };
+            return View("Yeni", model);
+        }
+        public ActionResult Kaydet(Operations operations)
+        {
+            if (operations.Id == 0)
+            {
+                db.Operations.Add(operations);
             }
             else
             {
-                var updatedEntity = db.Entry(students);
+                var updatedEntity = db.Entry(operations);
                 updatedEntity.State = System.Data.Entity.EntityState.Modified;
             }
             db.SaveChanges();
@@ -36,21 +41,22 @@ namespace LibraryMVCProjects.Controllers
         }
         public ActionResult Update(int id)
         {
-            var updatedStudent = db.Students.Find(id);
-            if (updatedStudent == null)
+            var model = new OperationViewModels()
             {
-                return HttpNotFound();
-            }
-            return View("Yeni", updatedStudent);
+                Books = db.Books.ToList(),
+                Students = db.Students.ToList(),
+                Operations = db.Operations.Find(id)
+            };
+            return View("Yeni", model);
         }
         public ActionResult Delete(int id)
         {
-            var deletedStudent = db.Students.Find(id);
-            if (deletedStudent == null)
+            var deletedOperation = db.Operations.Find(id);
+            if (deletedOperation == null)
             {
                 return HttpNotFound();
             }
-            db.Students.Remove(deletedStudent);
+            db.Operations.Remove(deletedOperation);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
---
+++ LibraryMVCProjects/ViewModels/OperationViewModels.cs
@@ -0,0 +1,15 @@
+﻿using LibraryMVCProjects.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryMVCProjects.ViewModels
+{
+    public class OperationViewModels
+    {
+        public List<Students> Students { get; set; }
+        public List<Books> Books { get; set; }
+        public Operations Operations { get; set; }
+    }
+}
