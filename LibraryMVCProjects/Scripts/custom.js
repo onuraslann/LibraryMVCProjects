4edf9876bed81d96242993726779ca61e44886bc
Onuraslann <theeaslann1@gmail.com>
2021-09-23 22:26:08 +0300

    Ajax ile silme işlemi gerçekleştirildi

---
+++ LibraryMVCProjects/Scripts/custom.js
@@ -0,0 +1,96 @@
+﻿$(function () {
+
+    $("#tblBookTypes").on("click", ".btnBookTypeDelete", function () {
+        var btn = $(this);
+        bootbox.confirm("Markayı silmek istediğinize eminmisiniz ? ", function (result) {
+            if (result) {
+                var id = btn.data("id");
+
+                $.ajax({
+
+                    type: "GET",
+                    url: "/BookType/Delete/" + id,
+                    success: function () {
+
+                        btn.parent().parent().remove();
+                    }
+
+
+                });
+            }
+
+        });
+    });
+})
+$(function () {
+
+    $("#tblOperations").on("click", ".btnOperationDelete", function () {
+        var btn = $(this);
+        bootbox.confirm("Markayı silmek istediğinize eminmisiniz ? ", function (result) {
+            if (result) {
+                var id = btn.data("id");
+
+                $.ajax({
+
+                    type: "GET",
+                    url: "/Operation/Delete/" + id,
+                    success: function () {
+
+                        btn.parent().parent().remove();
+                    }
+
+
+                });
+            }
+
+        });
+    });
+})
+$(function () {
+
+    $("#tblStudent").on("click", ".btnStudentDelete", function () {
+        var btn = $(this);
+        bootbox.confirm("Markayı silmek istediğinize eminmisiniz ? ", function (result) {
+            if (result) {
+                var id = btn.data("id");
+
+                $.ajax({
+
+                    type: "GET",
+                    url: "/Student/Delete/" + id,
+                    success: function () {
+
+                        btn.parent().parent().remove();
+                    }
+
+
+                });
+            }
+
+        });
+    });
+})
+$(function () {
+
+    $("#tblWriters").on("click", ".btnWriterDelete", function () {
+        var btn = $(this);
+        bootbox.confirm("Markayı silmek istediğinize eminmisiniz ? ", function (result) {
+            if (result) {
+                var id = btn.data("id");
+
+                $.ajax({
+
+                    type: "GET",
+                    url: "/Writer/Delete/" + id,
+                    success: function () {
+
+                        btn.parent().parent().remove();
+                    }
+
+
+                });
+            }
+
+        });
+    });
+})
